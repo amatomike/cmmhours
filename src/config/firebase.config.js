fde19e68c507f5aa2f52c7ce64602f9deb12fe59
Mike Amato <mike@mikeamato.org>
2016-09-21 16:53:27 -0600

    login components

---
+++ src/config/firebase.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    apiKey: "AIzaSyAP5RigcKf5F10nPCEsnpfyrZ1m6W6NaN8",
+    authDomain: "cmmdaily.firebaseapp.com",
+    databaseURL: "https://cmmdaily.firebaseio.com",
+    storageBucket: "cmmdaily.appspot.com",
+    messagingSenderId: "544116357337"
+}
--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+ReactDOM.render(routes , document.getElementById('root'));
+
+//ReactDOM.render(
+//    <App />,
+//    document.getElementById('root')
+//);
