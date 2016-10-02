0253fa3c6c9eb2abcd03e7420f26810fa6d9dfa3
mike <mike@mikeamato.org>
2016-10-02 10:25:43 -0600

    reorganization

--- old/ref/material/src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- * Copyright (c) 2016. Mike Amato, All Rights Reserved.
- ******************************************************************************/
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
-}
